990be150d4b394b398d363b13b9cfe1c326996d8
MARCO RIZZUTO <mprizzuto88@gmail.com>
2019-10-09 17:18:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var kittens = ['Milo', 'Otis', 'Garfield'] //define your array here
 // Add your functions and code here
 function destructivelyAppendKitten(name){
  //var name='ralph';
- return kittens.push(name);
+  kittens.push(name);
 }
 
 function destructivelyPrependKitten(name){
